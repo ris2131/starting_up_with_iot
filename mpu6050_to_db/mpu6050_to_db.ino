ccfa6ae3c768e1df8d8a1b6af5394205a500dff7
noranbanana <61417726+noranbanana@users.noreply.github.com>
2020-04-30 19:09:20 +0900

    Update mpu6050_to_db.ino

--- mpu6050_to_db/mpu6050_to_db.ino
+++ mpu6050_to_db/mpu6050_to_db.ino
@@ -2,13 +2,15 @@
 #include <Wire.h>
 #include <WiFiManager.h>
 #include <HTTPClient.h>
+#include <string.h>
 #define USE_SERIAL Serial
+
 
 MPU6050 mpu6050(Wire);
 
 long timer = 0;
 
-char url[100];
+char url[1000];
 
 void setup(void)
 {
@@ -30,21 +32,28 @@ void setup(void)
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
 }
+int loop_count = 0;
+char append_value[100];
 
 void loop(void)
 { 
   mpu6050.update();
-  
-  Serial.print("accX : ");Serial.print(mpu6050.getAccX());
-  Serial.print("\taccY : ");Serial.print(mpu6050.getAccY());
-  Serial.print("\taccZ : ");Serial.println(mpu6050.getAccZ());
-  
-  HTTPClient http;
-  sprintf(url, "http://54.146.59.56:8000/data?id=1&accX=%f&accY=%f&accZ=%f", mpu6050.getAccX(),mpu6050.getAccY(),mpu6050.getAccZ());
-  
-  http.begin(url);
-  int httpCode = http.GET();
-
+  //Serial.print("accX : ");Serial.print(mpu6050.getAccX());
+  //accx[loop_count] = mpu6050.getAccX();
+  //Serial.print("\taccY : ");Serial.print(mpu6050.getAccY());
+  //accy[loop_count] = mpu6050.getAccY();
+  //Serial.print("\taccZ : ");Serial.println(mpu6050.getAccZ());
+  //accz[loop_count] = mpu6050.getAccZ();
+  if(loop_count == 0){
+    sprintf(url, "http://54.146.59.56:8000/data?id=2");  
+  }
+  sprintf(append_value, "&accX%d=%f&accY%d=%f&accZ%d=%f", loop_count, mpu6050.getAccX(), loop_count, mpu6050.getAccY(), loop_count, mpu6050.getAccZ());
+  strcat(url, append_value);
+  loop_count++;
+  if(loop_count == 10){
+    HTTPClient http;
+    http.begin(url);
+    int httpCode = http.GET();
     // httpCode will be negative on error
     if(httpCode > 0) {
         // HTTP header has been send and Server response header has been handled
@@ -58,5 +67,7 @@ void loop(void)
         USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
     http.end();
-  delay(1000);
+    loop_count = 0;
+  }
+  delay(100);
 }
